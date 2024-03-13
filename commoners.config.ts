4bcb76299fef00c21e9a07a97941e5c69d8ebb3b
Garrett Michael Flynn <garrettmflynn@gmail.com>
2024-03-13 13:43:59 -0700

    Simplify build and fix workflows

--- commoners.config.ts
+++ commoners.config.ts
@@ -1,17 +1,4 @@
-// ------------- PRODUCTION -------------
 import * as bluetoothPlugin from '@commoners/bluetooth'
-import localServicesPlugin from '@commoners/local-services'
-// import { defineConfig } from 'commoners' // NOTE: commoners dependencies are missing in local development...
-
-// // ------------- DEVELOPMENT -------------
-// import * as bluetoothPlugin from '../commoners/packages/plugins/devices/ble/index.js'
-// import * as serialPlugin from '../commoners/packages/plugins/devices/serial/index.js'
-// import localServicesPlugin from '../commoners/packages/plugins/local-services/index.js'
-// import { defineConfig } from '../commoners/packages/core/index' // NOTE: commoners dependencies are missing in local development...
-
-// ------------- PACKAGE CHANGES -------------
-// "commoners": "file:../commoners/packages/cli",
-
 
 const defineConfig = (o) => o 
 
@@ -31,33 +18,6 @@ export default defineConfig({
 
     plugins: {
         bluetooth: bluetoothPlugin,
-        localServices: localServicesPlugin((ip, env) => {
-            const isLocalIP = ip === 'localhost'
-            const hasAuthString = process.env.SHARE_SECRET_KEY === env.SHARE_SECRET_KEY
-            return hasAuthString || isLocalIP
-        }),
-
-        selectiveBuilds:{
-            isSupported: {
-                desktop: {
-                    load: true
-                },
-                mobile: {
-                    load: true
-                },
-                web: {
-                    load: false
-                }
-            },
-            main: () => {
-                console.log(`desktop build (main)`)
-            },
-            preload: () => {
-                console.log(commoners.target + ' build (preload)')
-            },
-            render: () => console.log(commoners.target + ' build (render)'),
-
-        }
     },
 
     services: {
@@ -66,10 +26,8 @@ export default defineConfig({
         python: {
             description: 'A simple Python server',
             src: './src/services/python/main.py',
-            port: 1111,
             publish: {
                 build: 'python -m PyInstaller --name flask --onedir --clean ./src/services/python/main.py --distpath ./build/python',
-                remote: 'https://python-production-4f11.up.railway.app',
                 local: {
                     src: 'flask',
                     base: './build/python/flask', // Will be copied
--- src/frontend/main.ts
+++ src/frontend/main.ts
@@ -1,54 +1,5 @@
 import { BleClient } from '@capacitor-community/bluetooth-le';
 
-
-const list = document.querySelector('#commands') as HTMLUListElement
-
-
-
-async function createClient(url: string | URL, id = '') {
-
-  url = new URL('.commoners', url)
-  
-  const client = await new SwaggerClient(typeof url === 'string' ? url : url.href).catch((e: any) => {
-    throw new Error(`Failed to create client for ${url}: ${e.message}`)
-  })
-
-  const nestedList = document.createElement('ul')
-  nestedList.id = id
-  list.appendChild(nestedList)
-
-
-  const { title, description } = client.spec.info
-  const section = document.createElement('section')
-  section.innerHTML = `<h3>${title}</h3><small>${description}</small>`
-  nestedList.append(section)
-
-  // Populate list of available methods
-  Object.keys(client.spec.paths).forEach((path: any) => {
-    const info = client.spec.paths[path]
-
-    const keys = Object.keys(info)
-    keys.forEach((method: any) => {
-      const { operationId, tags, description } = info[method]
-      const li = document.createElement('li') 
-      const container = document.createElement('div')     
-      container.innerHTML = `${operationId ?? path}${keys.length > 1 ? ` (${method})` : ''}<br>${description ? `<small>${description}</small><br/>` : ''} <div class="tags">${tags.map(tag => `<div>${tag}</div>`)}</div>`
-
-      const button = document.createElement('button')
-      button.innerText = 'Run'
-      button.onclick = async () => {
-          const result = await client.apis[tags[0]][operationId]()
-          onData({ source: title, command: operationId, payload: result.body })
-      }
-
-      li.append(container, button)
-      nestedList.appendChild(li)
-    })
-  })
-
-  return client
-}
-
 const messages = document.getElementById('messages') as HTMLElement
 
 const display = (message: string) => {
@@ -62,60 +13,6 @@ const onData = (data: any) => {
   console.log(data)
   display(`${data.source ? `${data.source} (${data.command})` : data.command} - ${JSON.stringify(data.payload)}`)
 }
-
-// Remote API Tests (Basic Fetch Commands)
-if (commoners.services.remote && commoners.services.dynamic) {
-    const remoteAPI = new URL('/users', commoners.services.remote.url)
-    const dynamicAPI = new URL('/users', commoners.services.dynamic.url)
-
-    setTimeout(() => {
-
-      fetch(remoteAPI)
-      .then(response => response.json())
-      .then(json => onData({source: 'Remote', command: 'users', payload: json.length}))
-      .catch(e => console.error('Failed to request from remote server', e))
-
-      fetch(dynamicAPI)
-      .then(response => response.json())
-      .then(json => onData({source: `Dynamic`, command: 'users', payload: json.length}))
-      .catch(e => console.error('Failed to request from dynamic server', e))
-    })
-
-} 
-
-
-
-
-// --------- Node Service Test (WebSockets) ---------
-const nodeServices = { LocalNode: commoners.services.localNode, DynamicNode: commoners.services.dynamicNode }
-
-console.log(commoners)
-Object.entries(nodeServices).forEach(([label, service]) => {
-
-  if (service) {
-    const url = new URL(service.url)
-
-    try {
-      const ws = new WebSocket(`ws://${url.host}`)
-
-      ws.onmessage = (o) => {
-        const data = JSON.parse(o.data)
-        onData({source: label, ...data})
-      }
-
-      let send = (o: any) => {
-        ws.send(JSON.stringify(o))
-      }
-
-      ws.onopen = () => {
-        send({ command: 'platform' })
-        send({ command: 'version' })
-      }
-    } catch (e) {
-      console.error('Failed to connect to Node.js server', e)
-    }
-  }
-})
 
 // --------- Python Service Test (OpenAPI) ---------
 if (commoners.services.python) {
@@ -143,28 +40,6 @@ if (commoners.services.python) {
 }
 
 commoners.ready.then(plugins => {
-
-  if ('localServices' in plugins) {
-
-    const localServices = plugins['localServices']
-    const ids: { [x:string]: string }  = {}
-
-    localServices.onFound((url) => {
-      if (ids[url]) return
-      const id = ids[url] = Math.random().toString(36).substring(7)
-      createClient(url, id)
-    })
-
-    localServices.onClosed((url) => {
-      const el = document.getElementById(ids[url])
-      if (el) el.remove()
-      delete ids[url]
-    })
-
-    localServices.get()
-
-  }
-
 
   // --------- Web Bluetooth Test ---------
   async function requestBluetoothDevice () {
