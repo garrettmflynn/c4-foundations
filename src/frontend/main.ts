8e85fd5461165dac7db25a4a92fe08ac64b26946
Garrett Michael Flynn <garrettmflynn@gmail.com>
2024-03-20 10:28:47 -0700

    Display all data values through UI elements

--- src/frontend/main.ts
+++ src/frontend/main.ts
@@ -1,6 +1,9 @@
-// import { MuseClient } from '../../../muse-capacitor/src/muse';
-import { MuseClient } from './libs/muse-capacitor/src/muse';
-// import './capacitor-polyfill'
+import uPlot from 'uplot';
+import { ACCELEROMETER_FREQUENCY, ACCELEROMETER_SAMPLES_PER_READING, EEG_FREQUENCY, EEG_SAMPLES_PER_READING, MuseClient, PPG_FREQUENCY, PPG_SAMPLES_PER_READING, TelemetryData } from 'muse-capacitor/src/muse';
+import { perc2color } from './utils';
+
+import 'uplot'
+import 'uplot/dist/uPlot.min.css'
 
 type DataType = {
   eeg?: {
@@ -12,23 +15,68 @@ type DataType = {
   acceleration?: any[]
 }
 
-import AcquireWorker from '../services/workers/acquisition.worker?worker'
-import AnalyzeWorker from '../services/workers/analysis.worker?worker'
+// ---------------------- Plot Options ----------------------
+const plotHeight = 400
+const uplotOpts = {
+  series: [
+    {},
+    {
+      // initial toggled state (optional)
+      show: true,
 
-// Share Worker Stuff
-const acquire = new AcquireWorker();
-const analyze = new AnalyzeWorker();
-const channel = new MessageChannel();
-acquire.postMessage({port: channel.port1}, [channel.port1]);
-analyze.postMessage({port: channel.port2}, [channel.port2]);
+      spanGaps: false,
 
-analyze.onmessage = (e) => {
-  display(`<b>Worker Analysis Output</b> - ${e.data}`, 'analysis-output')
-}
+      // in-legend display
+      label: "Reading",
+      // value: (self, rawValue) => "$" + rawValue.toFixed(2),
 
-// Main thread
-const messages = document.getElementById('messages') as HTMLElement
+      // series style
+      stroke: "red",
+      width: 1,
+      // fill: "rgba(255, 0, 0, 0.3)",
+      // dash: [10, 5],
+    }
+  ],
+};
 
+
+// ---------------------- Worker threads ----------------------
+
+// import AcquireWorker from '../services/workers/acquisition.worker?worker'
+// import AnalyzeWorker from '../services/workers/analysis.worker?worker'
+
+// // Share Worker Stuff
+// const acquire = new AcquireWorker();
+// const analyze = new AnalyzeWorker();
+// const channel = new MessageChannel();
+// acquire.postMessage({port: channel.port1}, [channel.port1]);
+// analyze.postMessage({port: channel.port2}, [channel.port2]);
+
+// analyze.onmessage = (e) => {
+//   console.warn(`Worker Analysis Output - ${e.data}`, 'analysis-output')
+// }
+
+// ---------------------- Main thread ----------------------
+
+const secondsToPlot = 4
+const nEEGSamplesToPlot = Math.round((EEG_FREQUENCY / EEG_SAMPLES_PER_READING) * secondsToPlot)
+const nPPGSamplesToPlot = Math.round((PPG_FREQUENCY / PPG_SAMPLES_PER_READING) * secondsToPlot)
+const nAccSamplesToPlot = Math.round((ACCELEROMETER_FREQUENCY / ACCELEROMETER_SAMPLES_PER_READING) * secondsToPlot)
+const accAxes = ['x', 'y', 'z']
+
+// Elements
+const graphs = document.getElementById('graphs')!
+const batteryLevel = document.getElementById('battery')!.children[0] as HTMLElement
+const temperature = document.getElementById('temperature') as HTMLElement
+const hwVersion = document.getElementById('hardwareVersion')!
+const fwVersion = document.getElementById('firmwareVersion')!
+
+const connectInfo = document.getElementById('connect') as HTMLDivElement
+const connectedInfo = document.getElementById('connected') as HTMLDivElement
+const connectButton = document.getElementById('connect-button') as HTMLButtonElement
+const disconnectButton = document.getElementById('disconnect-button') as HTMLButtonElement
+
+// Muse Connection
 const connectToMuse = async (onchange = ( name: string, info: any, data: any ) => {}) => {
 
   const indexToChannel = (index: number) => {
@@ -57,26 +105,35 @@ const connectToMuse = async (onchange = ( name: string, info: any, data: any ) =
     const channel = indexToChannel(reading.electrode)
     if (!data.eeg[channel]) data.eeg[channel] = []
     const update  = { eegTimestamp: reading.timestamp, timestamp: now, samples: reading.samples }
-    data.eeg[channel].push(update)
+
+    const updatedData = data.eeg[channel]
+    updatedData.push(update)
+
     onchange('eeg', {
       channel,
       ...update
-    }, data)
+    }, updatedData)
+
   });
 
-  client.telemetryData.subscribe(telemetry => {
+  client.telemetryData.subscribe((telemetry: TelemetryData) => {
     const now = Date.now()
+
     if (!data.telemetry) data.telemetry = {}
+
     for (let key in telemetry) {
       if (key === 'sequenceId') continue
       if (!data.telemetry[key]) data.telemetry[key] = []
 
       const update = { timestamp: now, value: telemetry[key] }
-      data.telemetry[key].push(update)
+
+      const updatedData = data.telemetry[key]
+      updatedData.push(update)
+
       onchange('telemetry', {
         key,
         ...update
-      }, data)
+      }, updatedData)
     }
   });
 
@@ -84,8 +141,9 @@ const connectToMuse = async (onchange = ( name: string, info: any, data: any ) =
     const now = Date.now()
     if (!data.acceleration) data.acceleration = []
     const update = { timestamp: now, samples: acceleration.samples }
-    data.acceleration.push(update)
-    onchange('acceleration', update, data)
+    const updatedData = data.acceleration
+    updatedData.push(update)
+    onchange('acceleration', update, updatedData)
   });
 
   client.ppgReadings.subscribe(reading => {
@@ -94,28 +152,48 @@ const connectToMuse = async (onchange = ( name: string, info: any, data: any ) =
     const channel = reading.ppgChannel
     if (!data.ppg[channel]) data.ppg[channel] = []
     const update  = { eegTimestamp: reading.timestamp, timestamp: now, samples: reading.samples }
-    data.ppg[channel].push(update)
+    const updatedData = data.ppg[channel]
+    updatedData.push(update)
     onchange('ppg', {
       channel,
       ...update
-    }, data)
+    }, updatedData)
   })
 
   return client
 }
 
-const display = (message: string, id?: string) => {
-  const existing = id ? document.getElementById(id) : null
+const plots: {[x:string]: uPlot} = {}
 
-  if (existing) existing.innerHTML = message
-  else messages.innerHTML += `<div id=${id}>${message}</div>`
+const updatePlotSize = (plot: uPlot) => plot.setSize({ width: graphs.offsetWidth, height: plotHeight })
 
-  // messages.scrollTop = messages.scrollHeight;
+document.body.onresize = () => Object.values(plots).forEach(plot => updatePlotSize(plot))
+
+const plot = (label: string, data: any) => {
+
+  // Create canvas if it doesn't exist
+  if (!plots[label]) {
+      const opts = {
+        ...uplotOpts,
+        id: label,
+        title: label
+      }
+      const plot = new uPlot(opts, undefined, graphs);
+      plots[label] = plot
+  }
+
+  // Plot the data
+  const plot = plots[label];
+  data[0] = data[0].map((n: any) => n / 1000) // Convert to seconds
+
+  updatePlotSize(plot)
+
+  plot.setData(data);
 }
 
 const onData = (data: any) => {
   if (data.error) return console.error(data.error)
-  display(`<b>${data.source ? `${data.source} (${data.command})` : data.command}</b> - ${JSON.stringify(data.payload)}`)
+  console.warn(`${data.source ? `${data.source} (${data.command})` : data.command} - ${JSON.stringify(data.payload)}`)
 }
 
 // --------- Python Service Test (OpenAPI) ---------
@@ -146,48 +224,89 @@ if (commoners.services.python) {
 commoners.ready.then(plugins => {
 
   // --------- Web Bluetooth Test ---------
-  const testBluetoothConnection = document.getElementById('testBluetoothConnection')
 
-  const onDataChanged = (name: string, info: any, data: any) => {
+  const onDataChanged = (name: string, info: any, updatedData: any) => {
 
     if (name === 'eeg') {
-      const { channel, samples } = info
-      const latestSample = samples[samples.length - 1]
-      display(`<b>EEG (${channel})</b> ${latestSample.toFixed(5)}`, `eeg-${channel}`)
+      const { 
+        channel, 
+        // samples 
+      } = info
+
+      const dataToPlot = updatedData.slice(-nEEGSamplesToPlot)
+      const formattedData = [
+        dataToPlot.map(o => o.timestamp), // Timestamps
+        dataToPlot.map(o => o.samples[o.samples.length - 1]) // Samples
+      ]
+      return plot(`EEG (${channel})`, formattedData)
     }
 
     else if (name === 'telemetry') {
       const { key, value } = info
-      display(`<b>Telemetry (${key})</b> ${value}`, `telemetry-${key}`)
+
+      // Update Battery Indicator
+      if (key === 'batteryLevel') return Object.assign(batteryLevel.style, {
+        width: `${value}%`,
+        background: perc2color(value)
+      })
+
+      else if (key === 'temperature') return temperature.innerHTML = `${value}°C`
     }
 
     else if (name === 'acceleration') {
-      const { samples } = info
-      const { x, y, z } = samples[samples.length - 1]
-      display(`<b>Acceleration</b>\nx: ${x.toFixed(5)}\ny: ${y.toFixed(5)}\nz: ${z.toFixed(5)}`, 'acceleration')
+      // const { samples } = info
+      const dataToPlot = updatedData.slice(-nAccSamplesToPlot)
+      const timestamps = dataToPlot.map(o => o.timestamp)
+      const latestSamples = dataToPlot.map(o => o.samples[o.samples.length - 1]) // Last sample
+
+      return accAxes.forEach((axis) => plot(`Acceleration ${axis}`, [
+        timestamps,
+        latestSamples.map((o: any) => o[axis])
+      ] ))
     }
 
     else if (name === 'ppg') {
-      const { channel, samples } = info
-      const latestSample = samples[samples.length - 1]
-      display(`<b>PPG (${channel})</b> ${latestSample.toFixed(5)}`, `ppg-${channel}`)
+      const { 
+        channel, 
+        // samples 
+      } = info
+
+      const dataToPlot = updatedData.slice(-nPPGSamplesToPlot)
+      const formattedData = [
+        dataToPlot.map(o => o.timestamp), // Timestamps
+        dataToPlot.map(o => o.samples[o.samples.length - 1]) // Samples
+      ]
+
+      return plot(`PPG (${channel})`, formattedData)
     }
 
-    else console.log('Uncaptured data', name, info, data)
+    console.log('Uncaptured data', name, info)
   }
 
-  if (testBluetoothConnection) {
-    if ('bluetooth' in plugins) testBluetoothConnection.addEventListener('click', async () => {
-      const client = await connectToMuse(onDataChanged)
-      const device = client.device
-      display(`<b>Connected to Bluetooth Device</b> - ${device.name || `ID: ${device.deviceId}`}`)
+  if ('bluetooth' in plugins) {
 
-      await client.deviceInfo().then(({ fw: firmwareVersion, hw: hardwareVersion }) => {
-        console.log('Device Info', firmwareVersion, hardwareVersion)
-      });
-    })
+      let client: MuseClient
+      connectButton.addEventListener('click', async () => {
+        client = await connectToMuse(onDataChanged)
 
-    else testBluetoothConnection.setAttribute('disabled', '')
-  }
+        connectInfo.setAttribute('hidden', '')
+        connectedInfo.removeAttribute('hidden')
+
+        await client.deviceInfo().then(({ fw, hw }) => {
+          hwVersion.innerHTML = hw
+          fwVersion.innerHTML = fw
+        });
+      })
+
+      disconnectButton.addEventListener('click', async () => {
+        if (!client) return
+        await client.disconnect()
+                
+        connectedInfo.setAttribute('hidden', '')
+        connectInfo.removeAttribute('hidden')
+
+      })
+
+  } else connectButton.setAttribute('disabled', '')
 
 })
